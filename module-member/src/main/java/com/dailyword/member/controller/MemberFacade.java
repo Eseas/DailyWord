cc46473ad716d3aeb2ae77610d0474091c372aac
Eseas <8snlf9@gmail.com>
2025-05-13 20:23:24 +0900

    refactor : change that Module-Member's Controller to Facade
    - internal controller is not named Controller. then change to 'facade'
    - MemberInfo Controller and Member Controller merge

--- module-member/src/main/java/com/dailyword/member/controller/MemberFacade.java
+++ module-member/src/main/java/com/dailyword/member/controller/MemberFacade.java
@@ -2,9 +2,13 @@ package com.dailyword.member.controller;
 
 import com.dailyword.common.response.APIResponse;
 import com.dailyword.member.dto.member.GetMemberInfo;
+import com.dailyword.member.dto.member.LoginDto;
 import com.dailyword.member.dto.member.PatchPassword;
+import com.dailyword.member.dto.member.RegisterMember;
 import com.dailyword.member.usecase.GetMemberInfoUseCase;
+import com.dailyword.member.usecase.LoginUseCase;
 import com.dailyword.member.usecase.PatchPasswordUseCase;
+import com.dailyword.member.usecase.RegisterMemberUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +16,14 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-public class MemberInfoController {
+public class MemberFacade {
 
+    private final LoginUseCase loginUseCase;
+    private final RegisterMemberUseCase registerMemberUseCase;
     private final PatchPasswordUseCase patchPasswordUseCase;
     private final GetMemberInfoUseCase getMemberInfoUseCase;
 
-    @GetMapping("/internal/member/{id}")
+    @GetMapping("/internal/members/{id}")
     public ResponseEntity<APIResponse<GetMemberInfo.Response>> getMemberInfo(
             @PathVariable Long id
     ) {
@@ -26,7 +32,25 @@ public class MemberInfoController {
         return ResponseEntity.status(HttpStatus.OK).body(APIResponse.success(response));
     }
 
-    @PatchMapping("/internal/member/password")
+    @PostMapping("/internal/auth/login")
+    public ResponseEntity<APIResponse<LoginDto.Response>> login(
+            LoginDto.Request requestDto
+    ) {
+        LoginDto.Response response = loginUseCase.login(requestDto);
+
+        return ResponseEntity.status(HttpStatus.OK).body(APIResponse.success(response));
+    }
+
+    @PostMapping("/internal/auth/members/signup")
+    public ResponseEntity<APIResponse<RegisterMember.Response>> signup(
+            RegisterMember.Request requestDto
+    ) {
+        RegisterMember.Response responseDto = registerMemberUseCase.register(requestDto.toCommand());
+
+        return ResponseEntity.status(HttpStatus.OK).body(APIResponse.success(responseDto));
+    }
+
+    @PatchMapping("/internal/members/password")
     public ResponseEntity patchPassword(
             @RequestBody PatchPassword.Request requestDto
     ) {
@@ -34,4 +58,5 @@ public class MemberInfoController {
 
         return ResponseEntity.ok().build();
     }
+
 }
