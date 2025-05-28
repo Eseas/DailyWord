6270752adca1b1cfa78ede5c9b01b8232bc476f6
Eseas <8snlf9@gmail.com>
2025-05-28 20:38:52 +0900

    setting : kakao module basic setting

---
+++ module-kakao/src/main/java/com/dailyword/kakao/ModuleKakaoApplication.java
@@ -0,0 +1,13 @@
+package com.dailyword.kakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleKakaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleKakaoApplication.class, args);
+    }
+
+}
---
+++ module-kakao/src/main/java/com/dailyword/kakao/facade/KakaoFacade.java
@@ -0,0 +1,27 @@
+package com.dailyword.kakao.facade;
+
+import com.dailyword.common.response.APIResponse;
+import com.dailyword.kakao.dto.KakaoTokenResponse;
+import com.dailyword.kakao.dto.KakaoUserInfoResponse;
+import com.dailyword.kakao.service.KakaoAuthServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class KakaoFacade {
+
+    private final KakaoAuthServiceImpl authService;
+
+    @PostMapping("/kakao/login")
+    public ResponseEntity<APIResponse<KakaoUserInfoResponse>> kakaoLogin(@RequestParam("username") String username) {
+        String accessToken = authService.getAccessToken(username);
+        KakaoUserInfoResponse userInfo = authService.getUserInfo(accessToken);
+
+        return ResponseEntity.status(HttpStatus.OK).body(APIResponse.success(userInfo));
+    }
+}
