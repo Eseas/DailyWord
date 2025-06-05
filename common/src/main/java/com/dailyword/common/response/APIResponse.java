a37c8fcabea19f2a7c15930bab8470c2b5bf44bd
sy.lee <8snlf9@micehub.com>
2025-06-05 09:53:58 +0900

    refactor : change Custom APIResponse

--- common/src/main/java/com/dailyword/common/response/APIResponse.java
+++ common/src/main/java/com/dailyword/common/response/APIResponse.java
@@ -1,33 +1,47 @@
 package com.dailyword.common.response;
 
 public class APIResponse<T> {
+    boolean success;
     int code;
     String message;
     T data;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 
     public T getData() {
         return data;
     }
 
-    private APIResponse(int code, String message, T data) {
+    private APIResponse(boolean success, int code, String message, T data) {
+        this.success = success;
         this.code = code;
         this.message = message;
         this.data = data;
     }
 
     public static <T> APIResponse<T> success(int code, T data) {
-        return new APIResponse<T>(code, "success", data);
+        return new APIResponse<T>(true, code, "success", data);
     }
 
     public static <T> APIResponse<T> success(T data) {
-        return new APIResponse<T>(200, "success", data);
+        return new APIResponse<T>(true,200, "success", data);
     }
 
     public static <T> APIResponse<T> success() {
-        return new APIResponse<T>(200, "success", null);
+        return new APIResponse<T>(true,200, "success", null);
     }
 
     public static <T> APIResponse<T> error(int code, String message) {
-        return new APIResponse<T>(code, message, null);
+        return new APIResponse<T>(true, code, message, null);
     }
 }
