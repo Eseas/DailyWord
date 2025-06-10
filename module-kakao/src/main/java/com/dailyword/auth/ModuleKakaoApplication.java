32c162b43d403be54be1ca637701d2c2d958fc65
Eseas <8snlf9@gmail.com>
2025-06-10 10:44:46 +0900

    setting : kakao application add Enable Configuration Properties annotation

--- module-kakao/src/main/java/com/dailyword/auth/ModuleKakaoApplication.java
+++ module-kakao/src/main/java/com/dailyword/auth/ModuleKakaoApplication.java
@@ -1,9 +1,12 @@
 package com.dailyword.auth;
 
+import com.dailyword.auth.config.KakaoProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KakaoProperties.class)
 public class ModuleKakaoApplication {
 
     public static void main(String[] args) {
