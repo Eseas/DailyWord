8d704538c5681e08fe8201dbd16796114fb34095
Eseas <8snlf9@gmail.com>
2025-06-11 13:26:03 +0900

    setting : module post create

---
+++ module-post/src/main/java/com/dailyword/member/ModulePostApplication.java
@@ -0,0 +1,13 @@
+package com.dailyword.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulePostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulePostApplication.class, args);
+    }
+
+}
