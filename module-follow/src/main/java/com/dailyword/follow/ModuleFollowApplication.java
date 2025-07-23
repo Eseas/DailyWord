374a234d6eac0c1d7ccaf9adf73d5984190a1fc2
Eseas <8snlf9@gmail.com>
2025-07-23 09:01:58 +0900

    setting : create follow module

---
+++ module-follow/src/main/java/com/dailyword/follow/ModuleFollowApplication.java
@@ -0,0 +1,13 @@
+package com.dailyword.follow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleFollowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleFollowApplication.class, args);
+    }
+
+}
