8858513d6cf8e6e90162480d9caae41ccc515162
Eseas <8snlf9@gmail.com>
2026-01-19 16:47:10 +0900

    setting : module - notification 세팅

---
+++ module-notification/src/main/java/com/dailyword/notification/ModuleAdminApplication.java
@@ -0,0 +1,12 @@
+package com.dailyword.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleNotificationApplication.class, args);
+    }
+}
